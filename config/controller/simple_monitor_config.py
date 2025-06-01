de1ccff8a93f31dd4d0cb2de84bc6ef3cd53acd0
p76121699 <p76121699@gs.ncku.edu.tw>
2025-06-01 13:43:30 +0800

    fix some file

--- config/controller/simple_monitor_config.py
+++ config/controller/simple_monitor_config.py
@@ -1,6 +1,6 @@
 config = {
-    "conda_sh": "/home/dcnlab1/miniconda3/etc/profile.d/conda.sh",
-    "conda_env": "sdn1",
-    "controller_dir": "/home/dcnlab1/refactor/utils",
+    "conda_sh": "/home/username/miniconda3/etc/profile.d/conda.sh",
+    "conda_env": "sdn1", 
+    "controller_dir": "/home/username/ls2ic_sdn_routing/utils",
     "controller_entry": "simple_monitor.py"
 }
